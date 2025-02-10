8a210c96399e60142b255db87950c475b7ee87f6
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-02-10 21:28:21 +0600

    add eye icon on password input box

--- src/pages/Ragistration.jsx
+++ src/pages/Ragistration.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import Banner from "../assets/ragistration/banner.png";
 import { registrationInputData } from "../Library/Registration";
+import { FaEye } from "react-icons/fa";
 const Ragistration = () => {
-
-    const [email , setEmail] = useState("")
-    const [fullname , setFullName] = useState("")
-    const [password , setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [fullname, setFullName] = useState("");
+  const [password, setPassword] = useState("");
+  const [eye, seteye] = useState(false);
   const item = registrationInputData();
 
   /**
@@ -14,14 +15,26 @@ const Ragistration = () => {
    * return : null
    */
 
-  const handleInput = (event)=>{
-    const {name, value} = event.target;
-    console.log(value);
-    
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name == "fullname") {
+      setFullName(value);
+    } else {
+      setPassword(value);
+    }
+  };
+
+  /**
+   * todo : handleEye function implement
+   * @param ()
+   */
+
+  const handleEye = () => {
+    seteye(!eye);
   }
 
-  console.log(email);
-  
   return (
     <div>
       <div className="flex">
@@ -30,27 +43,49 @@ const Ragistration = () => {
             <div>
               <h1>Get started with easily register</h1>
               <p className="mb-3">Free register and you can enjoy it</p>
-              {item?.map((item) => (
-                <div className="flex flex-col">
-                  <label htmlFor="#">
-                    Your {item.name} <span className="text-red-400">*</span>
-                  </label>
-                  <input
-                    className="py-2 px-3 border border-gray-400 rounded"
-                    type={
-                      item.name.toLocaleLowerCase() == "email"
-                        ? "Email".toLocaleLowerCase()
-                        : item.name == "fullname"
-                        ? "text"
-                        : "password"
-                    }
-                    name={item.name}
-                    id={item.id}
-                    placeholder={`Enter your ${item.name}`}
-                    onChange={handleInput}
-                  />
-                </div>
-              ))}
+              {item?.map((item) =>
+                item.name == "password" ? (
+                  <div className="flex flex-col relative">
+                    <label htmlFor="#">
+                      Your {item.name} <span className="text-red-400">*</span>
+                    </label>
+                    <input
+                      className="py-2 px-3 border border-gray-400 rounded"
+                      type={eye ? "text" : "password"}
+                      name={item.name}
+                      id={item.id}
+                      placeholder={`Enter your ${item.name}`}
+                      onChange={handleInput}
+                    />
+                    <span
+                      className="absolute right-[10px] top-[36px] cursor-pointer"
+                      onClick={handleEye}
+                    >
+                      <FaEye />
+                    </span>
+                  </div>
+                ) : (
+                  <div className="flex flex-col">
+                    <label htmlFor="#">
+                      Your {item.name} <span className="text-red-400">*</span>
+                    </label>
+                    <input
+                      className="py-2 px-3 border border-gray-400 rounded"
+                      type={
+                        item.name.toLocaleLowerCase() == "email"
+                          ? "Email".toLocaleLowerCase()
+                          : item.name == "fullname"
+                          ? "text"
+                          : "password"
+                      }
+                      name={item.name}
+                      id={item.id}
+                      placeholder={`Enter your ${item.name}`}
+                      onChange={handleInput}
+                    />
+                  </div>
+                )
+              )}
             </div>
           </div>
         </div>
