2a29f932a62704da4b1f491198e43d79da4633e8
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-03-20 19:05:44 +0600

    add handle upload  profile picture

--- src/pages/DashboardComponents/Sideber.jsx
+++ src/pages/DashboardComponents/Sideber.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/dashboard/Ellipse1.png";
 import { FaCommentDots, FaRegBell, FaSignOutAlt } from "react-icons/fa";
 import { FiUpload } from "react-icons/fi";
@@ -7,31 +7,72 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { Link, useLocation } from "react-router-dom";
 
 const Sideber = () => {
+  const [profilePic, setProfilePic] = useState(null);
+
   const location = useLocation();
   console.log(location.pathname);
   const navigationIcon = [
     {
       id: 1,
-      path : "/dashboard",
+      path: "/dashboard",
       icon: <AiOutlineHome />,
     },
     {
       id: 2,
-      path : "/dashboard/messages",
+      path: "/dashboard/messages",
       icon: <FaCommentDots />,
     },
     {
       id: 3,
-      path : "/dashboard/notification",
+      path: "/dashboard/notification",
       icon: <FaRegBell />,
     },
     {
       id: 4,
-      path : "/dashboard/settings",
+      path: "/dashboard/settings",
       icon: <IoSettingsOutline />,
     },
   ];
 
+  /**
+   * todo : handleUploadProfile function implement
+   * @param ()
+   */
+  const handleUploadProfile = () => {
+    cloudinary.openUploadWidget({
+      cloudName: "df6bqehwu",
+      uploadPreset: "ChattingApp",
+      sources: [ 'local', "image_search"],
+      googleApiKey: 'AIzaSyDNBBcpHmRv8loQmTw7MkciBDPtsipumAM',
+      searchBySites: ["all", "cloudinary.com"],
+      searchByRights: true,
+    }, (error, result)=>{
+      if(error){
+        throw new Error("Failed to upload profile picture");
+      }else if (result.event === "success") {
+        setProfilePic(result.info.secure_url)
+        // const imageUrl = result.info.secure_url;
+        //   setProfilePic(imageUrl);
+        //   localStorage.setItem("profilePic", imageUrl);
+      }
+    })
+  };
+
+
+  useEffect(()=>{
+    const script = document.createElement("script")
+    script.src = "https://upload-widget.cloudinary.com/latest/global/all.js"
+    script.async = true;
+    document.body.appendChild(script)   
+
+    // const savedProfilePic = localStorage.getItem("profilePic");
+    // if (savedProfilePic) {
+    //   setProfilePic(savedProfilePic);
+    // }
+
+  }, [])
+
+  console.log(window.cloudinary);
   
   return (
     <div className="sidebar py-6 flex flex-col items-center justify-between bg-blueColor w-[12%] h-full rounded-2xl shadow-lg border-gray-200 border-[1px] border-solid">
@@ -39,17 +80,30 @@ const Sideber = () => {
         <div className="relative group w-[100px] h-[100px] rounded-full overflow-hidden">
           <img
             className="relative group w-[100px] h-[100px] rounded-full overflow-hidden"
-            src={logo}
+            src={profilePic || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'}
             alt=""
           />
-          <div className="cursor-pointer absolute rounded-full inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity duration-300"></div>
-          <span className="cursor-pointer absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-3xl hidden group-hover:block">
+          <div
+            
+            className="cursor-pointer absolute rounded-full inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity duration-300"
+          ></div>
+          <span onClick={handleUploadProfile} className="cursor-pointer p-8 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-3xl hidden group-hover:block">
             <FiUpload />
           </span>
         </div>
         <div className="menu w-full flex flex-col items-center justify-center gap-[20px] mt-[60px] ">
           {navigationIcon?.map((item, index) => (
-            <Link to={item.path} className={location.pathname == item.path ? ("active w-full py-[15px] flex justify-center text-[40px] text-[#BAD1FF] cursor-pointer") : ("w-full py-[15px] flex justify-center text-[40px] text-[#BAD1FF] cursor-pointer")} key={item.id}>{item.icon}</Link>
+            <Link
+              to={item.path}
+              className={
+                location.pathname == item.path
+                  ? "active w-full py-[15px] flex justify-center text-[40px] text-[#BAD1FF] cursor-pointer"
+                  : "w-full py-[15px] flex justify-center text-[40px] text-[#BAD1FF] cursor-pointer"
+              }
+              key={item.id}
+            >
+              {item.icon}
+            </Link>
           ))}
         </div>
       </div>
